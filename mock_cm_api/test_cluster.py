64656d12ebc508110e929f24a30848d57f26a4ff
Sebin John <sebin.john.sebin@gmail.com>
2015-10-20 18:08:57 +1100

    updating mocks

--- mock_cm_api/test_cluster.py
+++ mock_cm_api/test_cluster.py
@@ -1,10 +1,10 @@
 from test_service import TestApiService
 
 class TestApiCluster(object):
-
+    available_services = {}
     def __init__(self, cluster_name):
         self.cluster_name = cluster_name
-        self.available_services = [
+        self.supported_services = [
             'ZooKeeper',
             'HDFS',
             'MapReduce',
@@ -16,18 +16,18 @@ class TestApiCluster(object):
         return self.cluster_name
 
     def get_all_services(self):
-        return self.services.keys()
+        return self.available_services.values()
 
     def get_service(service_name):
-        return self.services[service_name]
+        return available_services[service_name]
 
     def create_service(service_name):
-        if service_name not in available_services:
+        if service_name not in supported_services:
             raise Exception('Not a supported service')
         try:
             service = TestApiService(service_name)
             service.name = service_name
-            self.services[service_name] = service
+            available_services[service_name] = service
             return service
         except Exception as e:
             return None
